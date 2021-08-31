4287c829d553bc9d65c726aff0306aa327073a5b
Jeff Inman <jeffery.inman@hotmail.com>
2021-08-30 21:29:20 -0700

    developing app for codefellows

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 'use strict';
 
-let random_number = Math.floor(Math.random() * 24)
-let random_saying = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.', 'As I see it', 'yes',
-'Ask again later',
-'Better not tell you now',
-'Cannot predict now',
-'Concentrate and ask again',
-'Don’t count on it',
-'It is certain',
-'It is decidedly so',
-'Most likely',
-'My reply is no',
-'My sources say no',
-'Outlook good',
-'Outlook not so good',
-'Reply hazy try again',
-'Signs point to yes',
-'Very doubtful',
-'Without a doubt',
-'Yes',
-'Yes, definitely',
-'You may rely on it'];
+let getMealBtn = document.getElementById('get_meal');
+let mealContainer = document.getElementById('meal');
 
-
-
-
+getMealBtn.addEventListener('click', () => {
+  fetch('https://www.affirmations.dev/')
+  .then(res => res.json())
+  .then(res => {
+    createMeal(res.meals[0])
+  })
+})
 
 console.log(random_saying[random_number])
 
---
+++ results.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let random_number = Math.floor(Math.random() * 24)
+let random_saying = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.', 'As I see it', 'yes', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don’t count on it', 'It is certain', 'It is decidedly so', 'Most likely', 'My reply is no', 'My sources say no', 'Outlook good', 'Outlook not so good', 'Reply hazy try again', 'Signs point to yes', 'Very doubtful', 'Without a doubt', 'Yes', 'Yes, definitely', 'You may rely on it'];
+
+
+
+
+
+console.log(random_saying[random_number])
