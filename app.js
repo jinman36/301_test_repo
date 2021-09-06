625fa36bfde1dfc24dd4f067b3f1144637de08d6
Jeff Inman <jeffery.inman@hotmail.com>
2021-09-06 16:05:08 -0700

    styled a little more, and started to pull the logic from the form to the js for use in the affirmation page

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 'use strict';
+var userName = document.getElementsByName("fname")
 
-let getMealBtn = document.getElementById('get_meal');
-let mealContainer = document.getElementById('meal');
 
-getMealBtn.addEventListener('click', () => {
-  fetch('https://www.affirmations.dev/')
-  .then(res => res.json())
-  .then(res => {
-    createMeal(res.meals[0])
-  })
-})
-
-console.log(random_saying[random_number])
-
+console.log(fname)
